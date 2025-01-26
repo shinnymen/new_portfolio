f3374d37eb9ad034d588f60ebb77d3c077f1337f
shinnymen <65742813+shinnymen@users.noreply.github.com>
2025-01-26 17:07:04 +0100

    build1

--- src/constants/index.js
+++ src/constants/index.js
@@ -66,7 +66,7 @@ export const PROJECTS = [
     image: project2,
     description:
       "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
-    technologies: ["HTML", "CSS", "Angular", "React", "MongoDB", "Axios", "Tailwind", "Firebase"],
+    technologies: ["HTML", "CSS", "Angular", "React", "MongoDB"],
   },
   {
     title: "On The Road Website & App",

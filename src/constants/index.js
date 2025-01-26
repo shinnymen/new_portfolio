6ffec0686bdef8cd5fff3560393368ea6c6c98ab
shinnymen <65742813+shinnymen@users.noreply.github.com>
2025-01-26 16:34:00 +0100

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,99 @@
+import project1 from "../assets/projects/project-1.png";
+import project2 from "../assets/projects/project-5.png";
+import project3 from "../assets/projects/project-3.webp";
+import project4 from "../assets/projects/project-4.png";
+
+export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2024 - Present",
+    role: "Engineer IT",
+    company: "Delinked",
+    description: `Full Stack WEB Development - AI Development & Implementation - WEB Cybersecurity. Startup company in an Incubator at EuraTechnologie.`,
+    technologies: ["Javascript", "Python", "Django", "React.js", "Next.js", "mongoDB", "Figma", "AI", "Pipecat"],
+  },
+  {
+    year: "2024",
+    role: "Administrator of the Systems and Networks",
+    company: "DIRISI",
+    description: `Currently working at the Joint Directorate of Defense Infrastructure Networks and Information Systems. Responsible for setting up a dedicated server for a new internet network and ensuring its security.`,
+    technologies: ["Linux", "DHCP-DNS-NIS", "VMware", "Cyber"],
+  },
+  {
+    year: "2024",
+    role: "Administrator of the Systems and Networks",
+    company: "Tysta Networks",
+    description: `Participated in the Radio Local Loop connection project for the leading poultry industry group in Senegal.
+    Managed enterprise network administration, including Cisco routers, switches, and relay antennas.`,
+    technologies: ["CISCO", "LINUX", "Git", "Figma", "Three.js", "GoogleMaps API"],
+  },
+  {
+    year: "2021 - 2022",
+    role: "Full Stack Developper",
+    company: "W3bCraft & Kid Coding",
+    description: `Establishment of a web development and computer programming school for children - Training module on Scratch, Arduino, and JavaScript - Creation of the school's website.`,
+    technologies: ["React","Redux", "Javascript", "Node.js", "Express.js", "MongoDB", "Git", "Figma", "Three.js", "GoogleMaps API"],
+  },
+  {
+    year: "2022 - 2022",
+    role: "Cybersecurity Analyst",
+    company: "ASERO WorldWide",
+    description: `Conducted security audits for government organizations in Senegal - Established a national Cyber Defense SOC for the Senegalese government - On-site support (Data Center, Bank, Power Plants) with a cybersecurity expert. Drafted the NIST framework - Prepared reports - Delivered presentations to the client.`,
+    technologies: ["NIST", "Kali Linux", "PHP", "Sqlite"],
+  },
+  {
+    year: "2021",
+    role: "Developper Front-End",
+    company: "Afreecom",
+    description: `Conducted security audits for government organizations in Senegal - Established a national Cyber Defense SOC for the Senegalese government - On-site support (Data Center, Bank, Power Plants) with a cybersecurity expert. Drafted the NIST framework - Prepared reports - Delivered presentations to the client.`,
+    technologies: ["Angular", "Node.js", "JavaScript"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "Kid Coding Website",
+    image: project1,
+    description:
+      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+  },
+  {
+    title: "Interform Africa Website & Blog",
+    image: project2,
+    description:
+      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
+    technologies: ["HTML", "CSS", "Angular", "React", "MongoDB", "Axios", "Tailwind", "Firebase"],
+  },
+  {
+    title: "On The Road Website & App",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "React Native", "MongoDB", "Javascript", "MongoDB"],
+  },
+  {
+    title: "W3bCraft Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "Javascript", "MongoDB"],
+  },
+  {
+    title: "Afreecom Website",
+    image: project4,
+    description:
+      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+    link: "https://afreecom.net",
+  },
+];
+
+export const CONTACT = {
+  address: "Paris, France",
+  phoneNo: "+33 7 64 36 02 49",
+  email: "alexandre.medor@gmail.com",
+};
